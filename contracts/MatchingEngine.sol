2bb298931f35e5cda75a1ab9205522f1f32a4224
Mason Elliott <mason.elliott12@gmail.com>
2021-07-13 13:39:49 +1200

    added import statments

--- contracts/MatchingEngine.sol
+++ contracts/MatchingEngine.sol
@@ -2,13 +2,22 @@
 
 pragma solidity >=0.4.22 <0.9.0;
 
+
+//Importing contract
+import "./Exchange.sol";
+
+//Import RBTree library
+import "./BokkyPooBahsRedBlackTreeLibrary.sol";
+
+
 /**
 @title Matching Engine Contract
 */
-contract MatchingEngine {
+contract MatchingEngine is Exchange {
 
     bool private EngineTrading = false;
 
+    //Sell -> Buy -> Orders for the token combo
     mapping (address => mapping(address => uint256)) orderBook;
 
 }
