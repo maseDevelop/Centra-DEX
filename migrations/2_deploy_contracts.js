28d8c95f813bf5d1c3a1af77e81b139d8778b580
Mason Elliott <mason.elliott12@gmail.com>
2021-07-14 17:24:25 +1200

    updated file contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ var Exchange = artifacts.require("../contracts/Exchange.sol");
 var MatchingEngine = artifacts.require("../contracts/MatchingEngine.sol");
 var Testtoken1 = artifacts.require("../contracts/Testtoken1.sol");
 var Testtoken2 = artifacts.require("../contracts/Testtoken2.sol");
+var TestBokkyPooBahsRedBlackTreeRaw = artifacts.require("../contracts/TestBokkyPooBahsRedBlackTreeRaw.sol");
 
 module.exports = function(deployer) {
  deployer.deploy(Exchange);
@@ -10,4 +11,5 @@ module.exports = function(deployer) {
  deployer.deploy(Testtoken1,1000);
  deployer.deploy(Testtoken2,1000);
  deployer.deploy(MatchingEngine);
+ deployer.deploy(TestBokkyPooBahsRedBlackTreeRaw);
 };
