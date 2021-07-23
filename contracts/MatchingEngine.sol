6fd203464a049be38b8664c4c69e2328b028df1e
Mason Elliott <mason.elliott12@gmail.com>
2021-07-23 18:51:46 +1200

    before merge

--- contracts/MatchingEngine.sol
+++ contracts/MatchingEngine.sol
@@ -52,7 +52,7 @@ contract MatchingEngine is Exchange {
 
     //Overwritten Functions
 
-    /*function makeOffer(uint _sell_amt, address _sell_token, uint _buy_amt, address _buy_token, uint256 _expires) public override returns (uint256 _id) {
+    function makeOffer(uint _sell_amt, address _sell_token, uint _buy_amt, address _buy_token, uint256 _expires) public override returns (uint256 _id) {
     
         //Calling base function - Creating the order
         _id = super.makeOffer(_sell_amt,_sell_token,_buy_amt,_buy_token,_expires);
@@ -152,7 +152,7 @@ contract MatchingEngine is Exchange {
                 ob.insert(_price, _id, _sell_token, _buy_token);
             }*/
         }
-    }*/
+    }
 
     function takeOffer(uint _order_id, uint _quantity) public override preventRecursion {
         //Calling base function
